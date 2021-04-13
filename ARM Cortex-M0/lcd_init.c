6050ee671f91bd2aaf8237fb0e0ab72a7f963e23
blive0321 <BrandonHao0321@gmail.com>
2021-04-13 21:58:09 +0800

    lcd_init

--- ARM Cortex-M0/lcd_init.c
+++ ARM Cortex-M0/lcd_init.c
@@ -43,9 +43,118 @@ void lcd_init(void){
     delay(1000000);
 
     lcd_cmd(0x38);
-  	lcd_cmd(0x0E);
-	lcd_cmd(0x80);
-	lcd_cmd(0x06);
-	lcd_print(0x00, "                ");
-	lcd_print(0x40, "                "); 
+  	lcd_cmd(0x0C);
+    lcd_cmd(0x80);
+    lcd_cmd(0x06);
+    lcd_print(0x00, "                ");
+    lcd_print(0x40, "                "); 
+
+
+    //年
+    //寫入CGRAM
+    lcd_cmd(CGRAM_location_1); //設定CGRAM = 0100 0000 代表CGRA第一個Pattern位址
+    
+    lcd_put(0x10);
+    lcd_put(0x1f);
+    lcd_put(0x2);
+    lcd_put(0xf);
+    lcd_put(0xa);
+    lcd_put(0x1f);
+    lcd_put(0x2);
+    lcd_put(0x00);
+
+
+    //月
+    //寫入CGRAM
+    lcd_cmd(CGRAM_location_2); 
+
+    lcd_put(0xf);
+    lcd_put(0x9);
+    lcd_put(0xf);
+    lcd_put(0x9);
+    lcd_put(0xf);
+    lcd_put(0x9);
+    lcd_put(0x1b);
+    lcd_put(0x00);
+    
+
+    //日
+    //寫入CGRAM
+    lcd_cmd(CGRAM_location_3); 
+
+    lcd_put(0xf);
+    lcd_put(0x9);
+    lcd_put(0x9);
+    lcd_put(0xf);
+    lcd_put(0x9);
+    lcd_put(0x9);
+    lcd_put(0xf);
+    lcd_put(0x00);
+    
+    //恐龍
+    //寫入CGRAM
+    lcd_cmd(CGRAM_location_4); 
+
+    lcd_put(0x7);
+    lcd_put(0x7);
+    lcd_put(0x7);
+    lcd_put(0x16);
+    lcd_put(0x1f);
+    lcd_put(0xa);
+    lcd_put(0xa);
+    lcd_put(0x00);
+    
+    
+    //樹
+    //寫入CGRAM
+    lcd_cmd(CGRAM_location_5); 
+
+    lcd_put(0x4);
+    lcd_put(0x5);
+    lcd_put(0x15);
+    lcd_put(0x15);
+    lcd_put(0x17);
+    lcd_put(0x1c);
+    lcd_put(0x4);
+    lcd_put(0x00);
+    
+    //愛心
+    //寫入CGRAM
+    lcd_cmd(CGRAM_location_6); 
+
+    lcd_put(0x0);
+    lcd_put(0xa);
+    lcd_put(0x1f);
+    lcd_put(0x1f);
+    lcd_put(0x1f);
+    lcd_put(0xe);
+    lcd_put(0x4);
+    lcd_put(0x00);
+
+    //往前箭頭
+    //寫入CGRAM
+    lcd_cmd(CGRAM_location_7); 
+
+    lcd_put(0x4);
+    lcd_put(0xe);
+    lcd_put(0x15);
+    lcd_put(0x4);
+    lcd_put(0x4);
+    lcd_put(0x4);
+    lcd_put(0x4);
+    lcd_put(0x00);
+    
+
+    //禁止通行sign
+    //寫入CGRAM
+    lcd_cmd(CGRAM_location_8); 
+
+    lcd_put(0xe);
+    lcd_put(0x1f);
+    lcd_put(0x1f);
+    lcd_put(0x11);
+    lcd_put(0x1f);
+    lcd_put(0x1f);
+    lcd_put(0xe);
+    lcd_put(0x00);
 }
